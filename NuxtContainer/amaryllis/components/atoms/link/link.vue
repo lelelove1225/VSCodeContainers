1d87ac663963c143f3f6ea66b94e2b72af495a89
lelelove1225 <m_oota9006@icloud.com>
2019-06-05 22:55:56 +0900

    prettier設定の変更とか

---
+++ NuxtContainer/amaryllis/components/atoms/link/link.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img :src="imgSrc" @click="onLinkClick" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    imageSrc: {
+      type: String,
+      default: ""
+    },
+    linkUrl: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      imgSrc: this.imageSrc,
+      url: this.linkUrl
+    };
+  },
+  methods: {
+    onLinkClick() {
+      window.location.href = this.linkUrl;
+    }
+  }
+};
+</script>
